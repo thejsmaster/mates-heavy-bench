999762f5780dfafc20a41d9084e06679d81ee3ca
Sean Freeman <sfreeman123456789@gmail.com>
2026-06-04 09:28:21 +0530

    bump mates-fullstack to 1.0.0-beta.20 (fix POST body consumption)

--- server/main.ts
+++ server/main.ts
@@ -1,1 +1,11 @@
-import{onRequest,setServerTimeout}from"mates-fullstack";setServerTimeout(60);onRequest(c=>{c.resHeaders["x-benchmark"]="1"});
+import { onRequest, setServerTimeout } from "mates-fullstack";
+
+setServerTimeout(60);
+
+onRequest((c) => {
+  c.resHeaders["x-benchmark"] = "1";
+  // Log all API requests for debugging
+  if (c.url?.startsWith("/api/")) {
+    console.log("[rpc]", c.method, c.url);
+  }
+});
